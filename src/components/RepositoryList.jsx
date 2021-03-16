c61f21b39edb7b0f2e987196ce133ec444e975b4
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-03-16 14:22:48 -0300

    Listing Repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -16,17 +16,17 @@ export function RepositoryList() {
       .then((response) => response.json())
       .then((data) => setRepositories(data));
   }, []);
-  console.log(repositories);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
