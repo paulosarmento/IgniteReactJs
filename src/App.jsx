10f84886b3e21941732239ba60d7ee2ee8814dab
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-03-16 08:58:13 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 export default function App() {
-  return <RepositoryList />;
+  return (
+    <div>
+      <RepositoryList />
+      <Counter />
+    </div>
+  );
 }
