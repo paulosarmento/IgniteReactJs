3c868c61ed52529b217420396fc32f6f50de7063
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-03-16 13:48:13 -0300

    Using useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 const repository = {
   name: "unform",
@@ -8,6 +9,15 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/paulosarmento/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
